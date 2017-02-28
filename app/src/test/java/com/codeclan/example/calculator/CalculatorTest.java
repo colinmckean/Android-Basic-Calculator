287c49a59e397001420bc6557283bac6b6fa03f5
Colin McKean <colinmckeandigital@gmail.com>
2017-02-28 18:09:42 +0000

    Add test for Calculator addition.

---
+++ app/src/test/java/com/codeclan/example/calculator/CalculatorTest.java
@@ -0,0 +1,22 @@
+package com.codeclan.example.calculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by user on 28/02/2017.
+ */
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+
+    public void before() {
+        calculator = new Calculator();
+    }
+//    divide, and multiply.
+}
